611925b4f5ca5a4b386cc9c634c08bd5c7f3ce11
tasos <tasos@192.168.0.123>
2018-09-03 19:37:03 +0300

    fix

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react'
+import PropTypes from 'prop-types'
 import DataGrid, {
   GroupPanel,
   Pager,
@@ -10,44 +11,81 @@ import DataGrid, {
   FilterRow,
   Editing,
   Export
-} from "devextreme-react/ui/data-grid";
+} from 'devextreme-react/ui/data-grid'
 
-const pageSizes = [10, 20, 50, 100];
+const pageSizes = [10, 20, 50, 100]
 
 class DataTable extends React.Component {
   constructor(props) {
-    super(props);
-    this.handleUpdate = this.handleUpdate.bind(this);
+    super(props)
+    this.handleUpdate = this.handleUpdate.bind(this)
+    this.handelDelete = this.handelDelete.bind(this)
+    this.handelInsert = this.handelInsert.bind(this)
   }
 
   handleUpdate(e) {
-    this.props.update(e.data);
+    this.props.update(e)
+  }
+
+  handelDelete(e) {
+    this.props.delete(e)
+  }
+
+  handelInsert(e) {
+    this.props.insert(e)
   }
 
   render() {
+    const { countField, sumFields } = this.props
+    let sumItems = null
+    if (sumFields) {
+      sumItems = this.props.sumFields.map(field => (
+        <TotalItem key={field} column={field} summaryType={'sum'} />
+      ))
+    }
     return (
       <DataGrid
         allowColumnReordering={true}
+        allowColumnResizing={true}
         showBorders={true}
         dataSource={this.props.data}
         showRowLines={true}
         onRowUpdated={this.handleUpdate}
+        onRowRemoved={this.handelDelete}
+        onRowInserted={this.handelInsert}
       >
         <Export enabled={true} />
         <GroupPanel visible={true} />
         <SearchPanel visible={true} highlightCaseSensitive={true} />
-        <Selection mode={"single"} />
+        <Selection mode={'single'} />
         <FilterRow visible={true} />
         {this.props.children}
         <Summary>
-          <TotalItem column={this.props.countField} summaryType={"count"} />
+          {countField != null && (
+            <TotalItem column={countField} summaryType={'count'} />
+          )}
+          {sumItems != null && sumItems}
         </Summary>
         <Pager allowedPageSizes={pageSizes} showPageSizeSelector={true} />
         <Paging defaultPageSize={20} />
-        <Editing mode="popup" allowUpdating="true" />
+        <Editing
+          mode="popup"
+          allowUpdating={this.props.update !== undefined}
+          allowDeleting={this.props.delete !== undefined}
+          allowAdding={this.props.insert !== undefined}
+        />
       </DataGrid>
-    );
+    )
   }
 }
 
-export default DataTable;
+DataTable.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+  countField: PropTypes.string,
+  sumFields: PropTypes.arrayOf(PropTypes.string),
+  update: PropTypes.func,
+  delete: PropTypes.func,
+  insert: PropTypes.func
+}
+
+export default DataTable
--- src/views/OrgNodes.js
+++ src/views/OrgNodes.js
@@ -1,112 +1,124 @@
-import React from "react";
-import { AppContext } from "../app-context";
-import { Column } from "devextreme-react/ui/data-grid";
-import notify from "devextreme/ui/notify";
-import DataTable from "../components/DataTable";
+import React from 'react'
+import { AppContext } from '../app-context'
+import { Column } from 'devextreme-react/ui/data-grid'
+import notify from 'devextreme/ui/notify'
+import DataTable from '../components/DataTable'
 
 class OrgNodes extends React.Component {
   constructor(props) {
-    super(props);
-    this.getData = this.getData.bind(this);
-    this.handleUpdate = this.handleUpdate.bind(this);
+    super(props)
+    this.getData = this.getData.bind(this)
+    this.handleUpdate = this.handleUpdate.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+    this.handleInsert = this.handleInsert.bind(this)
     this.state = {
       data: [],
       nodeLevel: 0
-    };
+    }
   }
 
   componentDidMount() {
-    const { location } = this.props;
-    const path = location.pathname.split("/")[1];
-    var typeId = 0;
+    const { location } = this.props
+    const path = location.pathname.split('/')[1]
+    var typeId = 0
     switch (path) {
-      case "companies":
+      case 'companies':
         this.setState({
-          path: "Εταιρίες"
-        });
-        typeId = 1;
-        break;
-      case "services":
+          path: 'Εταιρίες'
+        })
+        typeId = 1
+        break
+      case 'services':
         this.setState({
-          path: "Υπηρεσίες"
-        });
-        typeId = 2;
-        break;
-      case "programs":
+          path: 'Υπηρεσίες'
+        })
+        typeId = 2
+        break
+      case 'programs':
         this.setState({
-          path: "Προγράμματα"
-        });
-        typeId = 3;
-        break;
-      case "areas":
+          path: 'Προγράμματα'
+        })
+        typeId = 3
+        break
+      case 'areas':
         this.setState({
-          path: "Περιοχές"
-        });
-        typeId = 4;
-        break;
-      case "actions":
+          path: 'Περιοχές'
+        })
+        typeId = 4
+        break
+      case 'actions':
         this.setState({
-          path: "Δράσεις"
-        });
-        typeId = 5;
-        break;
-      case "menu":
+          path: 'Δράσεις'
+        })
+        typeId = 5
+        break
+      case 'menu':
         this.setState({
-          path: "Μενού"
-        });
-        typeId = 6;
-        break;
-      case "submenu":
+          path: 'Μενού'
+        })
+        typeId = 6
+        break
+      case 'submenu':
         this.setState({
-          path: "Υπομενού"
-        });
-        typeId = 7;
-        break;
+          path: 'Υπομενού'
+        })
+        typeId = 7
+        break
       default:
-        break;
+        break
     }
-    this.setState({ nodeLevel: typeId - 1 });
-    this.getData(typeId);
+    this.setState({ nodeLevel: typeId - 1 })
+    this.getData(typeId)
   }
 
   getData(typeId) {
-    fetch("http://localhost:5000/api/orgnodes/type/" + typeId, {
-      method: "GET"
+    fetch('http://localhost:5000/api/orgnodes/type/' + typeId, {
+      method: 'GET'
     })
       .then(res => Promise.all([res, res.json()]))
       .then(([res, json]) => {
         if (res.ok) {
-          return json;
+          return json
         }
-        throw Error(json.Message);
+        throw Error(json.Message)
       })
       .then(res => {
-        this.setState({ data: res, filteredData: res });
-        notify("data fetched", "sucess", 600);
+        this.setState({ data: res, filteredData: res })
+        notify('data fetched', 'sucess', 600)
       })
       .catch(e => {
-        notify(e.message, "error", 600);
-      });
+        notify(e.message, 'error', 600)
+      })
   }
 
   handleUpdate(data) {
-    console.log(data);
+    console.log(data)
+  }
+
+  handleDelete(data) {
+    console.log(data)
+  }
+
+  handleInsert(data) {
+    console.log(data)
   }
 
   render() {
     return (
       <DataTable
         data={this.state.data}
-        countField={"title"}
+        countField={'title'}
         update={this.handleUpdate}
+        delete={this.handleDelete}
+        insert={this.handleInsert}
       >
-        <Column dataField={"title"} />
-        <Column dataField={"code"} />
-        <Column dataField={"isActive"} />
-        <Column dataField={"viewOrder"} />
-        <Column dataField={"appObject"} />
+        <Column dataField={'title'} />
+        <Column dataField={'code'} />
+        <Column dataField={'isActive'} />
+        <Column dataField={'viewOrder'} />
+        <Column dataField={'appObject'} />
       </DataTable>
-    );
+    )
   }
 }
 
@@ -114,4 +126,4 @@ export default props => (
   <AppContext.Consumer>
     {context => <OrgNodes {...props} context={context} />}
   </AppContext.Consumer>
-);
+)
