42217f43ae7d2675df5953f79d929b5d65d4c7fb
akkgr <akkgr@users.noreply.github.com>
2018-09-10 20:23:28 +0300

    fix

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
-import OrgNodes from "./views/OrgNodes";
-import OrgNode from "./views/OrgNode";
-import Login from "./views/Login";
+import OrgNodes from './views/OrgNodes'
+import OrgNode from './views/OrgNode'
 
 const indexRoutes = [
-  { path: "/login", component: Login },
-  { path: "/companies", component: OrgNodes },
-  { path: "/services", component: OrgNodes },
-  { path: "/programs", component: OrgNodes },
-  { path: "/areas", component: OrgNodes },
-  { path: "/actions", component: OrgNodes },
-  { path: "/menu", component: OrgNodes },
-  { path: "/submenu", component: OrgNodes },
-  { path: "/orgnode/:id", component: OrgNode }
-];
+  { path: '/companies', component: OrgNodes },
+  { path: '/services', component: OrgNodes },
+  { path: '/programs', component: OrgNodes },
+  { path: '/areas', component: OrgNodes },
+  { path: '/actions', component: OrgNodes },
+  { path: '/menu', component: OrgNodes },
+  { path: '/submenu', component: OrgNodes },
+  { path: '/orgnode/:id', component: OrgNode }
+]
 
-export default indexRoutes;
+export default indexRoutes
